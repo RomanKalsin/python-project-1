77d4e5138e9bbb0192b1353195d5374eb38df415
Roman Kalsin <karen52rus@gmail.com>
2026-01-13 17:09:40 +0300

    add brain_even

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,4 +1,5 @@
 import prompt
+import random
 
 
 def welcome_user():
@@ -8,8 +9,37 @@ def welcome_user():
     return user_name
 
 
+def is_even(num):
+    if num % 2 == 0:
+        return 'yes'
+    else:
+        return 'no'
+        
+
+def brain_even():
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    count = 0
+    while count < 3:
+        num = random.randint(1,100)
+        print(f'Question: {num}')
+        user_ansver = prompt.string('Your answer: ')
+        if is_even(num) != user_ansver:
+            return False
+        count += 1
+    return True
+
+
+def finish_game(result, user_name):
+    if result == True:
+        print(f"Congratulations, {user_name}!")
+    elif result == False:
+        print(f"Let's try again, {user_name}!")
+
+
 def main():
-    welcome_user()
+    user_name = welcome_user()
+    result = brain_even()
+    finish_game(result, user_name)
 
 
 if __name__ == '__main__':
